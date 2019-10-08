aa6716d9f0967073662d5fb217bec7910fe5ead1
Guilherme Ferreira <guifrribeiro@gmail.com>
2019-10-08 00:01:02 -0300

    Atualização de View (arrow function and lexical scope

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,12 +5,22 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        
+        this._listaNegociacoes = new ListaNegociacoes((model) =>
+            this._negociacoesView.update(model));
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
     }
 
@@ -20,7 +30,6 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso!';
         this._mensagemView.update(this._mensagem);
-        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
     }
 
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,21 @@
 class ListaNegociacoes {
 
-    constructor() {
+    constructor(armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha; // função que será passada por parâmetro
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
 
     get negociacoes() {
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        this._armadilha(this);
     }
 }
